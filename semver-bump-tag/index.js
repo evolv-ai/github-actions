63f5ebdc2db32f8bd55f78497bebabcb7c7f8e58
Frazer Bayley <14934024+FrazerBayley@users.noreply.github.com>
2020-06-04 15:34:38 -0700

    New approach (#1)
    * feat: try together
    
    * fix: take out tags input
    
    * fix: prefix and suffix

--- semver-bump-tag/index.js
+++ semver-bump-tag/index.js
@@ -6,29 +6,12 @@ const MINOR = 'minor';
 const PATCH = 'patch';
 const SEMVER_VERSIONS = [MAJOR, MINOR, PATCH];
 
-function generateNewTag(tags, bump, prefix, suffix) {
-	const regex = new RegExp(`^refs\\/tags\\/${prefix}(?<major>\\d+).(?<minor>\\d+).(?<patch>\\d+)$`);
-
-	const	semvers = tags
-		.map(tag => regex.exec(tag.ref))
-		.filter(re => re)
-		.map(re => {
-			const groups = re.groups;
-			groups.major = parseInt(groups.major);
-			groups.minor = parseInt(groups.minor);
-			groups.patch = parseInt(groups.patch);
-			return groups;
-		});
-
+function generateNewTag(semvers, bump, prefix, suffix) {
 	let maxNum = 0;
 	let maxSemver = null;
 	semvers.map(semver => {     
 			if (semver[bump] >= maxNum) maxSemver = semver;    
 	});
-
-	if (!maxSemver) {
-		return undefined;
-	}
 
 	maxSemver[bump] = maxSemver[bump] + 1;
 	const newTag = `${prefix}${maxSemver.major}.${maxSemver.minor}.${maxSemver.patch}${suffix}`;
@@ -40,24 +23,50 @@ async function run() {
 	try {
 		const context = github.context;
 		const token = core.getInput('token');
-		const tags = JSON.parse(core.getInput('tags'));
 		const bump = core.getInput('bump');
 		const prefix = core.getInput('prefix');
 		const suffix = core.getInput('suffix');
+		const major = core.getInput('major')
+		const minor = core.getInput('minor')
 
 		if (SEMVER_VERSIONS.indexOf(bump) === -1) {
 			throw Error('`bump` input must be either `major`, `minor`, or `patch`.');
 		}
-
-		const newTag = generateNewTag(tags, bump, prefix, suffix);
-
-		if (!newTag) {
-			console.log('Could not generate new tag.')
-			core.setOutput('tag', '');
-			return;
-		}
 		
 		const client = new github.GitHub(token);
+
+		let ref = `tags/${prefix}`;
+		if (major) {
+			ref += `${major}.`
+		} 
+		if (minor) {
+			ref += `${minor}.`
+		}
+
+		const tags = (await client.git.listMatchingRefs({
+			...context.repo,
+			ref 
+		})).data;
+
+		console.log(tags);
+
+		const regex = new RegExp(`^refs\\/tags\\/${prefix}(?<major>\\d+).(?<minor>\\d+).(?<patch>\\d+)$`);
+
+		const	semvers = tags
+			.map(tag => regex.exec(tag.ref))
+			.filter(re => re)
+			.map(re => {
+				const groups = re.groups;
+				groups.major = parseInt(groups.major);
+				groups.minor = parseInt(groups.minor);
+				groups.patch = parseInt(groups.patch);
+				return groups;
+			});
+
+		let newTag = `${prefix}${major || 1}.${minor || 0}.0${suffix}`
+		if (semvers.length > 0) {
+			newTag = generateNewTag(semvers, bump, prefix, suffix);
+		}
 
 		const resp = await client.git.createRef({
 			...context.repo,
