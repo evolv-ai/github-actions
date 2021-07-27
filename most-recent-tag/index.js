e6c666779ca166dee5b2de19ef7172b730a5fc29
Matt Strom <matt.strom@evolv.ai>
2021-07-27 16:00:53 -0700

    feat: add action for retrieving most recent tag

---
+++ most-recent-tag/index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+
+async function run() {
+	try {
+		const { context } = github;
+
+		const token = core.getInput('token');
+		const client = new github.GitHub(token);
+		const { owner, repo } = context.repo;
+
+		const { data } = await client.repos.listTags({ ...context.repo });
+
+		if (!data || !data[0]) {
+			throw new Error(`Error retrieving tags for '${owner}/${repo}'`);
+		}
+
+		const [mostRecent] = data;
+
+		core.setOutput('mostRecent', mostRecent);
+	} catch (error) {
+		core.error(error);
+		core.setFailed(error.message);
+	}
+}
+
+run();
